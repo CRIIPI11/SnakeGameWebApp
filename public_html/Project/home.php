345c882b23af615ad38460d3278e29dd944d1e3f
cristhian <cdml2001@hotmail.com>
2023-03-03 16:14:32 -0500

    Fixed Home Page
    Home page now includes game along with information depending of the user login status. Added Footer file in partials folder

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,8 +1,9 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php");
+
 ?>
 <div class="mb-3">
-    <h1 class="fw-bold mb-2 text-uppercase">Home</h1>
+    <h1 class="fw-bold mb-2 text-uppercase">Snake Game</h1>
 </div>
 <?php
 if (is_logged_in()) {
@@ -11,40 +12,50 @@ if (is_logged_in()) {
     //comment this out if you don't want to see the session variables
     //echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 }
+
 ?>
 
-<div class="row align-items-start">
-    <div class="col">
-        <?php $scoresw = get_top_week(); ?>
-        <div class="w-25 p-3">
+<div class="container">
+    <?php require(__DIR__ . "/Game/game.php"); ?>
+</div>
 
-            <div class="container">
-                <div class="row">
-                    <div class="span5">
-                        <h3>Top Week Scores</h3>
-                        <table class="table table-success">
-                            <thead>
-                                <th>Username</th>
-                                <th>Score</th>
-                                <th>Time</th>
-                            </thead>
-                            <tbody>
-                                <?php
-                                foreach ($scoresw as $score) : ?>
-                                    <tr>
-                                        <td><a href="<?php echo get_url("profile.php?id=");
-                                                        se($score, "user_id"); ?>"><?php se($score, "username"); ?></a></td>
-                                        <td><?php se($score, "score", 0); ?></td>
-                                        <td><?php se($score, "created", "-"); ?></td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
+
+<div class="row align-items-start">
+
+    <?php if (is_logged_in()) : ?>
+        <div class="col">
+            <?php $scoresw = get_top_week(); ?>
+            <div class="w-25 p-3">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="span5">
+                            <h3>Top Week Scores</h3>
+                            <table class="table table-success">
+                                <thead>
+                                    <th>Username</th>
+                                    <th>Score</th>
+                                    <th>Time</th>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    foreach ($scoresw as $score) : ?>
+                                        <tr>
+                                            <td><a href="<?php echo get_url("profile.php?id=");
+                                                            se($score, "user_id"); ?>"><?php se($score, "username"); ?></a></td>
+                                            <td><?php se($score, "score", 0); ?></td>
+                                            <td><?php se($score, "created", "-"); ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    <?php endif; ?>
+
 
 
     <div class="col">
@@ -119,4 +130,5 @@ if (is_logged_in()) {
 </style>
 <?php
 require(__DIR__ . "/../../partials/flash.php");
+require(__DIR__ . "/../../partials/footer.php");
 ?>
