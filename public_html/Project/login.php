dc367731391620b449f171e409d540630ec339c9
criipi <cdml2001@hotmail.com>
2023-03-21 19:30:15 -0400

    edited footer

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -1,13 +1,12 @@
 <?php
 require(__DIR__ . "/../../partials/nav.php"); ?>
-<div class="container py-5 h-100">
+<div class="container h-100">
     <div class="row d-flex justify-content-center align-items-center h-100">
-        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
             <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                <div class="card-body p-5 text-center">
+                <div class="card-body p-5">
                     <h1 class="fw-bold mb-2 text-uppercase">Login</h1>
                     <div class="mb-md-5 mt-md-4 pb-5">
-
                         <form onsubmit="return validate(this)" method="POST">
                             <div class="form-outline form-white mb-4">
                                 <input class="form-control form-control-lg" placeholder="Username/Email" type="text" id="email" name="email" required />
@@ -40,5 +39,6 @@ require(__DIR__ . "/../../partials/nav.php"); ?>
 
     ?>
     <?php
+    require(__DIR__ . "/../../partials/footer.php");
     require(__DIR__ . "/../../partials/flash.php");
     ?>
